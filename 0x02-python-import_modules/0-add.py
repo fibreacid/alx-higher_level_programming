ff9f63331fceb011b5d63ba4ab726587896e4925
Thabo Ramodike <thabzinrmk@gmail.com>
2025-05-14 01:57:54 -0700

    first task of project python modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+__import__("0-add")
+
+
+a = 1
+b = 2
+print(f"{a} + {b} = {a + b}")
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first interger
+        b: second interger
+
+    Returns:
+        the return value. a + b
+    """"
+    return (a + b)
